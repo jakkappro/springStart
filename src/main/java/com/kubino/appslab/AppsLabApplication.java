f3df8c78ca4cca8a29c15bb1e09f845dd2f5bb87
jakkappro <jakkappro@gmail.com>
2021-12-02 08:33:49 +0100

    Done

---
+++ src/main/java/com/kubino/appslab/AppsLabApplication.java
@@ -0,0 +1,14 @@
+package com.kubino.appslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppsLabApplication.class, args);
+        System.out.println("Hello Spring boot!");
+    }
+
+}
---
+++ src/test/java/com/kubino/appslab/AppsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubino.appslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppsLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
